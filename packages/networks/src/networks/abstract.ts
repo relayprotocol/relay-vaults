0c65d99be314c6f0cadfc6ec0b27adf04b209750
Clément Renaud <clement+git@clementrenaud.com>
2025-03-05 21:54:13 +0100

    fix rc and bridges

--- packages/networks/src/networks/abstract.ts
+++ packages/networks/src/networks/abstract.ts
@@ -9,6 +9,12 @@ export const abstract: L2NetworkConfig = {
   slug: 'abstract',
   isZKsync: true,
   hyperlaneMailbox: '0x9BbDf86b272d224323136E15594fdCe487F40ce7',
+  bridges: {
+    zksync: {
+      l1SharedDefaultBridge: '0xD7f9f54194C633F36CCD5F3da84ad4a1c38cB2cB',
+      l2SharedDefaultBridge: '0x954ba8223a6BFEC1Cc3867139243A02BA0Bc66e4',
+    },
+  },
   assets: {
     usdc: '0x84A71ccD554Cc1b02749b35d22F684CC8ec987e1',
     weth: '0x3439153EB7AF838Ad19d56E1571FBD09333C2809',
--- packages/networks/src/networks/zero.ts
+++ packages/networks/src/networks/zero.ts
@@ -9,9 +9,15 @@ export const zero: L2NetworkConfig = {
   slug: 'zero',
   isZKsync: true,
   hyperlaneMailbox: '0xd7b351D2dE3495eA259DD10ab4b9300A378Afbf3',
+  bridges: {
+    zksync: {
+      l1SharedDefaultBridge: '0xD7f9f54194C633F36CCD5F3da84ad4a1c38cB2cB',
+      l2SharedDefaultBridge: '0x954ba8223a6BFEC1Cc3867139243A02BA0Bc66e4',
+    },
+  },
   assets: {
-    usdc: '', // TODO: Add USDC address
-    weth: '', // TODO: Add WETH address
+    usdc: '0x6a6394F47DD0BAF794808F2749C09bd4Ee874E70',
+    weth: '0xAc98B49576B1C892ba6BFae08fE1BB0d80Cf599c',
   },
   rpc: ['https://zero.drpc.org'],
 }

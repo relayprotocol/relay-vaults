c50d88396987e5a5ff88985904adae8bc17303b6
Clément Renaud <clement+git@clementrenaud.com>
2025-03-12 15:36:03 +0100

    zksync withdrawal helper (#217)
    * zksync withdrawal helper
    
    * basic test
    
    * test native ok
    
    * erc20 test ok
    
    * remove extra blank

---
+++ packages/helpers/src/zksync.ts
@@ -0,0 +1,20 @@
+import { Provider, types, L1Signer } from 'zksync-ethers'
+import { JsonRpcSigner } from 'ethers'
+
+/**
+ * Finalizes a ZKSync withdrawal transaction
+ * @param signer - The signer that will finalize the withdrawal
+ * @param withdrawalHash - The hash of the withdrawal transaction to finalize
+ * @param network - The network to use (defaults to Sepolia)
+ * @returns The params
+ */
+export async function finalizeZksyncWithdrawal(
+  signer: JsonRpcSigner,
+  withdrawalHash: string,
+  network: types.Network = types.Network.Mainnet
+) {
+  const zksyncProvider = Provider.getDefaultProvider(network)
+  const l1Signer = L1Signer.from(signer, zksyncProvider)
+  const params = await l1Signer.finalizeWithdrawalParams(withdrawalHash)
+  return params
+}

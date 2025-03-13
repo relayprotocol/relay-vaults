f7ed7eed6386d0cba6d55fe9a2fe5a171c602240
Julien Genestoux <julien.genestoux@gmail.com>
2025-03-13 16:29:37 -0400

    undue change

--- smart-contracts/contracts/RelayPoolFactory.sol
+++ smart-contracts/contracts/RelayPoolFactory.sol
@@ -48,6 +48,11 @@ contract RelayPoolFactory {
     uint timelockDelay,
     uint256 initialDeposit
   ) public returns (address) {
+    // We require an initial deposit of 1 unit
+    uint8 decimals = ERC20(asset).decimals();
+    if (initialDeposit < 10 ** decimals) {
+      revert InsufficientInitialDeposit(initialDeposit);
+    }
     address[] memory curator = new address[](1);
     curator[0] = msg.sender;
 

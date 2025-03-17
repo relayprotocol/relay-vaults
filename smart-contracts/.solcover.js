207b4358928b81a2e912f8718a5fbb94b6c8dddd
Clément Renaud <clement+git@clementrenaud.com>
2025-03-17 13:03:48 +0100

    add tasks

--- smart-contracts/.solcover.js
+++ smart-contracts/.solcover.js
@@ -1,12 +1,3 @@
 module.exports = {
-  skipFiles: [
-    './utils',
-    './interfaces',
-    // covered in forked tests
-    './BridgeProxy/ArbitrumOrbitNativeBridgeProxy.sol',
-    './BridgeProxy/CCTPBridgeProxy.sol',
-    './BridgeProxy/OPStackNativeBridgeProxy.sol',
-    './BridgeProxy/ZkSyncBridgeProxy.sol',
-    './TokenSwap.sol',
-  ],
+  skipFiles: ['./utils', './interfaces'],
 }

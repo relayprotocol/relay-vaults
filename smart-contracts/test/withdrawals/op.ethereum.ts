52d1b72c8ae28468410bc81a812faf2bb6a36df3
Clément Renaud <clement+git@clementrenaud.com>
2025-03-17 17:33:27 +0100

    fix erc20

--- smart-contracts/test/withdrawals/op.ethereum.ts
+++ smart-contracts/test/withdrawals/op.ethereum.ts
@@ -1,13 +1,11 @@
-import { ethers, ignition } from 'hardhat'
+import { ethers } from 'hardhat'
 import { networks } from '@relay-protocol/networks'
 import { time } from '@nomicfoundation/hardhat-network-helpers'
-import {
-  buildProveWithdrawal,
-  buildFinalizeWithdrawal,
-} from '@relay-protocol/helpers'
+import { buildFinalizeWithdrawal, getEvent } from '@relay-protocol/helpers'
 import { expect } from 'chai'
 import * as ABIs from '@relay-protocol/helpers/abis'
 import { IOptimismPortal } from '../../typechain-types'
+import { ZeroAddress } from 'ethers'
 
 const {
   bridges: { op },
@@ -111,47 +109,11 @@ describe('op withdrawal helper', function () {
     )
     const receipt = await tx2.wait()
 
-    // Verify the events
-    receipt.logs.forEach((log: Log) => {
-      expect(log.address).to.be.oneOf([
-        '0x99C9fc46f92E8a1c0deC1b1747d010903E884bE1', // L1StandardBridgeProxy
-        '0x25ace71c97B33Cc4729CF772ae268934F7ab5fA1', // L1CrossDomainMessengerProxy
-        op.portalProxy, // OptimismPortalProxy
-      ])
-      if (log.address === '0x99C9fc46f92E8a1c0deC1b1747d010903E884bE1') {
-        const iface = new ethers.Interface(ABIs.L1StandardBridge)
-        const event = iface.parseLog(log)
-        expect(event.name).to.equal('ERC20BridgeFinalized')
-        expect(event.args.localToken).to.equal(
-          '0x7F5c764cBc14f9669B88837ca1490cCa17c31607'
-        )
-        expect(event.args.remoteToken).to.equal(
-          '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
-        )
-        expect(event.args.from).to.equal(
-          '0xF5C28ce24Acf47849988f147d5C75787c0103534'
-        )
-        expect(event.args.to).to.equal(
-          '0xF5C28ce24Acf47849988f147d5C75787c0103534'
-        )
-        expect(event.args.amount).to.equal(1000000000000000n)
-        expect(event.args.extraData).to.equal('0x7375706572627269646765')
-      } else if (log.address === '0x25ace71c97B33Cc4729CF772ae268934F7ab5fA1') {
-        const iface = new ethers.Interface(ABIs.L2CrossDomainMessenger)
-        const event = iface.parseLog(log)
-        expect(event.name).to.equal('RelayedMessage')
-        expect(event.args.msgHash).to.equal(
-          '0x9e16c5899d5e4e83897fa35082e0a65ef4b7853bdc573f9a8a3ed645ce4bb473'
-        )
-      } else if (log.address === op.portalProxy) {
-        const iface = new ethers.Interface(ABIs.Portal2)
-        const event = iface.parseLog(log)
-        expect(event.name).to.equal('WithdrawalFinalized')
-        expect(event.args.withdrawalHash).to.equal(
-          '0x332288a193bbd08ed40a2dadc309e7588d94a9d6ab28c9dc17655eef64e38e34'
-        )
-        expect(event.args.success).to.equal(true)
-      }
-    })
+    const { interface: iface } = await ethers.getContractAt(
+      'IOptimismPortal',
+      ZeroAddress
+    )
+    const { args } = await getEvent(receipt!, 'WithdrawalFinalized', iface)
+    expect(args.success).to.equal(true)
   })
 })

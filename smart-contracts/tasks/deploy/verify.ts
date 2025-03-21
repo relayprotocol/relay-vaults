3b300173d04f45aca1c728dc030decedea07aca1
Julien Genestoux <julien.genestoux@gmail.com>
2025-03-21 11:11:12 -0400

    deployed Bridge Factory on Lisk

--- smart-contracts/tasks/deploy/verify.ts
+++ smart-contracts/tasks/deploy/verify.ts
@@ -1,10 +1,18 @@
 import { task } from 'hardhat/config'
-
+import networks from '@relay-protocol/networks'
 task(
   'deploy:verify',
   'Verifies a contract utility, includes retries and wait times'
-).setAction(async ({ address, constructorArguments }, { ethers }) => {
+).setAction(async ({ address, constructorArguments }, { config, ethers }) => {
   const { chainId } = await ethers.provider.getNetwork()
+
+  if (!config.etherscan.apiKey[networks[chainId].name]) {
+    console.error(
+      `No Etherscan API key found for ${networks[chainId].name}. Please add one to hardhat.config.ts`
+    )
+    return
+  }
+
   if (chainId === 31337n) {
     // Not verifying on hardhat
     return
@@ -25,8 +33,8 @@ task(
         verified = true
       })
       .catch(async (e) => {
-        console.error(e)
         if (attempts >= 10) {
+          console.error(e)
           throw e
         }
         await new Promise((resolve) => setTimeout(resolve, 3000))

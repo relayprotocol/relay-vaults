e30f3be96cf2600052519ce9729e5985026d8337
Julien Genestoux <julien.genestoux@gmail.com>
2025-04-08 15:08:25 -0400

    adding code to solve

--- claimer/main.ts
+++ claimer/main.ts
@@ -1,10 +1,12 @@
 import { start, stop } from './src/runner'
 import { proveTransactions } from './src/prove-transactions'
 import { claimTransactions } from './src/claim-withdrawals'
+import { finalizeWithdrawals } from './src/finalize-withdrawals'
 
 const run = async () => {
   const { vaultService } = await start()
   await proveTransactions({ vaultService })
+  await finalizeWithdrawals({ vaultService })
   await claimTransactions({ vaultService })
   await stop()
   console.log('Done!')
---
+++ claimer/src/finalize-withdrawals.ts
@@ -0,0 +1,7 @@
+import { RelayVaultService } from '@relay-protocol/client'
+
+export const finalizeWithdrawals = async (_params: {
+  vaultService: RelayVaultService
+}) => {
+  // Do stuff!
+}

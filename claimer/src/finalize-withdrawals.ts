81ebfc8954112695455c962ef300ae7400eba7d5
Julien Genestoux <julien.genestoux@gmail.com>
2025-04-09 14:02:29 -0400

    finalize

--- claimer/src/finalize-withdrawals.ts
+++ claimer/src/finalize-withdrawals.ts
@@ -1,7 +1,60 @@
 import { RelayVaultService } from '@relay-protocol/client'
+import { gql } from 'graphql-request'
+import { finalizeWithdrawal } from './relay'
 
-export const finalizeWithdrawals = async (_params: {
+// TODO: handle time? Each chain has its own delay
+const GET_ALL_TRANSACTIONS_TO_FINALIZE = gql`
+  query GetAllBridgeTransactionsToProve(
+    $nativeBridgeStatus: String!
+    $originTimestamp: BigInt!
+  ) {
+    bridgeTransactions(
+      where: {
+        nativeBridgeStatus: $nativeBridgeStatus
+        originTimestamp_lt: $originTimestamp
+      }
+    ) {
+      items {
+        originBridgeAddress
+        nonce
+        originChainId
+        destinationPoolAddress
+        destinationPoolChainId
+        originSender
+        destinationRecipient
+        asset
+        amount
+        hyperlaneMessageId
+        nativeBridgeStatus
+        opProofTxHash
+        nativeBridgeFinalizedTxHash
+        loanEmittedTxHash
+        originTimestamp
+        originTxHash
+      }
+    }
+  }
+`
+
+export const finalizeWithdrawals = async ({
+  vaultService,
+}: {
   vaultService: RelayVaultService
 }) => {
-  // Do stuff!
+  const { bridgeTransactions } = await vaultService.query(
+    GET_ALL_TRANSACTIONS_TO_FINALIZE,
+    {
+      nativeBridgeStatus: 'INITIATED',
+      originTimestamp:
+        Math.floor(new Date().getTime() / 1000) - 60 * 60 * 24 * 7,
+    }
+  )
+  for (let i = 0; i < bridgeTransactions.items.length; i++) {
+    try {
+      const bridgeTransaction = bridgeTransactions.items[i]
+      // await finalizeWithdrawal(bridgeTransaction)
+    } catch (error) {
+      console.error(error)
+    }
+  }
 }

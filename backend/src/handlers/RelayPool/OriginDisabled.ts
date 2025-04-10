61dfaab6299735b70b67eb598f2177e0002989d4
Julien Genestoux <julien.genestoux@gmail.com>
2025-04-10 11:53:11 -0400

    deleting origin

--- backend/src/handlers/RelayPool/OriginDisabled.ts
+++ backend/src/handlers/RelayPool/OriginDisabled.ts
@@ -9,39 +9,11 @@ export default async function ({
   context: Context<'RelayPool:OriginDisabled'>
 }) {
   const poolAddress = event.log.address
-  console.log(event, poolAddress)
 
-  // uint32 chainId,
-  // address bridge,
-  // uint256 maxDebt,
-  // uint256 outstandingDebt,
-  // address proxyBridge
-
-  // await context.db.delete(poolOrigin).where({
-  //   originBridge: origin.bridge as `0x${string}`,
-  //   originChainId: origin.chainId,
-  //   pool: poolAddress as `0x${string}`,
-  // })
-
-  // Insert the pool origin
-  // await context.db
-  //   .insert(poolOrigin)
-  //   .values({
-  //     bridgeFee: origin.bridgeFee,
-  //     chainId: context.network.chainId,
-  //     coolDown: origin.coolDown,
-  //     curator: origin.curator,
-  //     currentOutstandingDebt: BigInt(0),
-  //     maxDebt: origin.maxDebt,
-  //     originBridge: origin.bridge as `0x${string}`,
-  //     originChainId: origin.chainId,
-  //     pool: poolAddress as `0x${string}`,
-  //     proxyBridge: origin.proxyBridge as `0x${string}`,
-  //   })
-  //   .onConflictDoUpdate({
-  //     bridgeFee: origin.bridgeFee,
-  //     coolDown: origin.coolDown,
-  //     curator: origin.curator,
-  //     maxDebt: origin.maxDebt,
-  //   })
+  await context.db.delete(poolOrigin, {
+    chainId: context.network.chainId,
+    originBridge: event.args.bridge as `0x${string}`,
+    originChainId: event.args.chainId,
+    pool: poolAddress as `0x${string}`,
+  })
 }

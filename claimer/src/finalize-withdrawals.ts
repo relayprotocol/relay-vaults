44d0db157b3b2a2a736d7e9438ba1aeb301c5e76
Julien Genestoux <julien.genestoux@gmail.com>
2025-04-11 10:55:31 -0400

    Finalize withdrawals (#296)
    * finalize
    
    * updated types
    
    * fixing types for backend
    
    * more types fixes
    
    * more types fixes
    
    * update default BACKEND_URL
    
    * fixed request

--- claimer/src/finalize-withdrawals.ts
+++ claimer/src/finalize-withdrawals.ts
@@ -1,7 +1,40 @@
 import { RelayVaultService } from '@relay-protocol/client'
+import { gql } from 'graphql-request'
+import { finalizeWithdrawal } from './relay'
 
-export const finalizeWithdrawals = async (_params: {
+// TODO: handle delays? Each chain has its own delay
+const GET_ALL_TRANSACTIONS_TO_FINALIZE = gql`
+  query GetAllBridgeTransactionsToProve($originTimestamp: BigInt!) {
+    bridgeTransactions(where: { originTimestamp_lt: $originTimestamp }) {
+      items {
+        originChainId
+        destinationPoolChainId
+        asset
+        amount
+        originTxHash
+      }
+    }
+  }
+`
+
+export const finalizeWithdrawals = async ({
+  vaultService,
+}: {
   vaultService: RelayVaultService
 }) => {
-  // Do stuff!
+  const { bridgeTransactions } = await vaultService.query(
+    GET_ALL_TRANSACTIONS_TO_FINALIZE,
+    {
+      originTimestamp:
+        Math.floor(new Date().getTime() / 1000) - 60 * 60 * 24 * 7,
+    }
+  )
+  for (let i = 0; i < bridgeTransactions.items.length; i++) {
+    try {
+      const bridgeTransaction = bridgeTransactions.items[i]
+      await finalizeWithdrawal(bridgeTransaction)
+    } catch (error) {
+      console.error(error)
+    }
+  }
 }

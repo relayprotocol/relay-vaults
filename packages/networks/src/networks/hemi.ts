2d19ee1711c5de89fe486dd6a14aa05747868297
Clément Renaud <clement+git@clementrenaud.com>
2025-04-15 15:26:00 +0200

    fix network files

--- packages/networks/src/networks/hemi.ts
+++ packages/networks/src/networks/hemi.ts
@@ -5,6 +5,7 @@ export const hemi: ChildNetworkConfig = {
     weth: '0x4200000000000000000000000000000000000006',
     // TODO: add USDC
   },
+  baseChainId: 1,
   bridges: {
     optimism: {
       l1: {
@@ -19,7 +20,6 @@ export const hemi: ChildNetworkConfig = {
   earliestBlock: 0, // Update with actual earliest block
   hyperlaneMailbox: '0x3a464f746D23Ab22155710f44dB16dcA53e0775E',
   isTestnet: false,
-  l1ChainId: 1,
   name: 'Hemi',
   rpc: process.env.RPC_43111
     ? [process.env.RPC_43111]
--- packages/networks/src/networks/rari.ts
+++ packages/networks/src/networks/rari.ts
@@ -5,6 +5,7 @@ export const rari: ChildNetworkConfig = {
   assets: {
     // TODO: add USDC
   },
+  baseChainId: 42161,
   bridges: {
     arbitrum: {
       l1: {
@@ -23,7 +24,6 @@ export const rari: ChildNetworkConfig = {
   earliestBlock: 0, // Update with actual earliest block
   hyperlaneMailbox: '0x65dCf8F6b3f6a0ECEdf3d0bdCB036AEa47A1d615',
   isTestnet: false,
-  l1ChainId: 42161,
   name: 'Rari',
   rpc: process.env.RPC_1380012617
     ? [process.env.RPC_1380012617]

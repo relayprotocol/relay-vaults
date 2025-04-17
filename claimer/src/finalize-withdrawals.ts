792c9115f3d5b3bcbc793871b8e392bc2fc245ed
Clément Renaud <clement+git@clementrenaud.com>
2025-04-17 23:16:57 +0200

    claimer use withdrawal delay

--- claimer/src/finalize-withdrawals.ts
+++ claimer/src/finalize-withdrawals.ts
@@ -1,11 +1,22 @@
 import { RelayVaultService } from '@relay-protocol/client'
 import { gql } from 'graphql-request'
 import { finalizeWithdrawal } from './relay'
+import networks from '@relay-protocol/networks'
+import { ChildNetworkConfig } from '@relay-protocol/types'
 
-// TODO: handle delays? Each chain has its own delay
+const SEVEN_DAYS = 60 * 60 * 24 * 7
+
 const GET_ALL_TRANSACTIONS_TO_FINALIZE = gql`
-  query GetAllBridgeTransactionsToProve($originTimestamp: BigInt!) {
-    bridgeTransactions(where: { originTimestamp_lt: $originTimestamp }) {
+  query GetAllBridgeTransactionsToProve(
+    $originTimestamp: BigInt!
+    $originChainId: Int!
+  ) {
+    bridgeTransactions(
+      where: {
+        originTimestamp_lt: $originTimestamp
+        originChainId: $originChainId
+      }
+    ) {
       items {
         originChainId
         destinationPoolChainId
@@ -22,16 +33,28 @@ export const finalizeWithdrawals = async ({
 }: {
   vaultService: RelayVaultService
 }) => {
-  const { bridgeTransactions } = await vaultService.query(
-    GET_ALL_TRANSACTIONS_TO_FINALIZE,
-    {
-      originTimestamp:
-        Math.floor(new Date().getTime() / 1000) - 60 * 60 * 24 * 7,
-    }
+  // query ready withdrawal for all chains
+  const bridgeTransactions = await Promise.all(
+    Object.values(networks)
+      .filter(({ chainId }) => chainId !== 1)
+      .map(async (network) => {
+        const { bridgeTransactions } = await vaultService.query(
+          GET_ALL_TRANSACTIONS_TO_FINALIZE,
+          {
+            // custom time for each chain
+            originTimestamp:
+              Math.floor(new Date().getTime() / 1000) -
+                (network as ChildNetworkConfig).withdrawalDelay! || SEVEN_DAYS, // 7 days for most chains
+          }
+        )
+        return bridgeTransactions.items
+      })
+      .flat()
   )
-  for (let i = 0; i < bridgeTransactions.items.length; i++) {
+
+  for (let i = 0; i < bridgeTransactions.length; i++) {
     try {
-      const bridgeTransaction = bridgeTransactions.items[i]
+      const bridgeTransaction = bridgeTransactions[i]
       await finalizeWithdrawal(bridgeTransaction)
     } catch (error) {
       console.error(error)

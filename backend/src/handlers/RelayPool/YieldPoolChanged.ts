bc39618061591aa3e043eef4f8bb4fb285962e8e
Julien Genestoux <17735+julien51@users.noreply.github.com>
2025-04-30 15:58:26 -0400

    handing YieldPoolChanged

---
+++ backend/src/handlers/RelayPool/YieldPoolChanged.ts
@@ -0,0 +1,21 @@
+import { eq } from 'ponder'
+import { Context, Event } from 'ponder:registry'
+import { relayPool } from 'ponder:schema'
+
+export default async function ({
+  event,
+  context,
+}: {
+  event: Event<'RelayPool:YieldPoolChanged'>
+  context: Context<'RelayPool:YieldPoolChanged'>
+}) {
+  await context.db.sql
+    .update(relayPool)
+    .set({
+      yieldPool: event.args.newPool as `0x${string}`,
+    })
+    .where(
+      eq(relayPool.chainId, context.network.chainId),
+      eq(relayPool.contractAddress, event.log.address as `0x${string}`)
+    )
+}

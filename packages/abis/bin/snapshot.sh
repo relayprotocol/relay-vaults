816e5fa9ba8d51459d5007577b242a19f2f14197
Clément Renaud <clement+git@clementrenaud.com>
2025-05-09 11:53:02 +0200

    add `yarn snapshot` command

---
+++ packages/abis/bin/snapshot.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# check arg
+if [ $# -eq 0 ]; then
+    echo "Error: Please provide a contract ABI path as argument"
+    exit 1
+fi
+
+filename=$1
+base=$(basename "$filename" .json)
+
+
+# find latest version number
+latest=0
+for f in src/versions/$base.*.json; do
+  if [ -f "$f" ]; then
+    ver=$(echo "$f" | grep -o '[0-9]*\.json' | grep -o '[0-9]*')
+    
+    if [ "$ver" -ge "$latest" ]; then
+      latest=$((ver + 1))
+    fi
+  fi
+done
+echo "Latest version number: $latest"
+# new filename with version number
+new_filename="$base.$latest.json"
+
+cp $filename "src/versions/$new_filename"
+echo "File $filename archived under $new_filename"

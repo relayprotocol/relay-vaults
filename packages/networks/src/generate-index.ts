f8347d940d0c38c3dcc2f9d7c7a56e9e876052d0
Julien Genestoux <17735+julien51@users.noreply.github.com>
2025-06-12 18:25:28 -0400

    adding json file that contains all the earliest blocks

--- packages/networks/src/generate-index.ts
+++ packages/networks/src/generate-index.ts
@@ -9,16 +9,26 @@ const files = fs
 const importLines: string[] = []
 const exportLines: string[] = []
 
+importLines.push(`import earliestBlocks from '../earliestBlocks.json'\n
+interface EarliestBlocks {
+  [key: string]: number
+}
+const typedEarliestBlocks: EarliestBlocks = earliestBlocks
+`)
+
 files.forEach((file) => {
   const fileNameWithoutExt = path.basename(file, '.ts')
   const importVar = fileNameWithoutExt.replace(/[^a-zA-Z0-9_$]/g, '_')
   importLines.push(`import config_${importVar} from './${fileNameWithoutExt}'`)
-  exportLines.push(
-    `export const ${fileNameWithoutExt} = { slug: '${fileNameWithoutExt}', ...config_${importVar} }`
-  )
+  exportLines.push(`export const ${fileNameWithoutExt} = {
+  slug: '${fileNameWithoutExt}',
+  ...config_${importVar},
+  earliestBlock: typedEarliestBlocks['${fileNameWithoutExt}'],
+}
+`)
 })
 
-const output = `// AUTO-GENERATED FILE. DO NOT EDIT.\n\n${importLines.join('\n')}\n\n${exportLines.join('\n')}\n`
+const output = `// AUTO-GENERATED FILE. DO NOT EDIT.\n\n${importLines.join('\n')}\n\n${exportLines.join('\n')}`
 
 fs.writeFileSync(path.join(configDir, 'index.ts'), output)
 console.log('✅ index.ts generated successfully')
--- packages/networks/src/networks/index.ts
+++ packages/networks/src/networks/index.ts
@@ -1,5 +1,12 @@
 // AUTO-GENERATED FILE. DO NOT EDIT.
 
+import earliestBlocks from '../earliestBlocks.json'
+
+interface EarliestBlocks {
+  [key: string]: number
+}
+const typedEarliestBlocks: EarliestBlocks = earliestBlocks
+
 import config_abstract from './abstract'
 import config_ancient8 from './ancient8'
 import config_arbitrumNova from './arbitrumNova'
@@ -20,22 +27,116 @@ import config_zero from './zero'
 import config_zksync from './zksync'
 import config_zora from './zora'
 
-export const abstract = { slug: 'abstract', ...config_abstract }
-export const ancient8 = { slug: 'ancient8', ...config_ancient8 }
-export const arbitrumNova = { slug: 'arbitrumNova', ...config_arbitrumNova }
-export const base = { slug: 'base', ...config_base }
-export const baseSepolia = { slug: 'baseSepolia', ...config_baseSepolia }
-export const bob = { slug: 'bob', ...config_bob }
-export const cyber = { slug: 'cyber', ...config_cyber }
-export const ethereum = { slug: 'ethereum', ...config_ethereum }
-export const hemi = { slug: 'hemi', ...config_hemi }
-export const lisk = { slug: 'lisk', ...config_lisk }
-export const mint = { slug: 'mint', ...config_mint }
-export const optimism = { slug: 'optimism', ...config_optimism }
-export const redstone = { slug: 'redstone', ...config_redstone }
-export const sepolia = { slug: 'sepolia', ...config_sepolia }
-export const soneium = { slug: 'soneium', ...config_soneium }
-export const swell = { slug: 'swell', ...config_swell }
-export const zero = { slug: 'zero', ...config_zero }
-export const zksync = { slug: 'zksync', ...config_zksync }
-export const zora = { slug: 'zora', ...config_zora }
+export const abstract = {
+  slug: 'abstract',
+  ...config_abstract,
+  earliestBlock: typedEarliestBlocks['abstract'],
+}
+
+export const ancient8 = {
+  slug: 'ancient8',
+  ...config_ancient8,
+  earliestBlock: typedEarliestBlocks['ancient8'],
+}
+
+export const arbitrumNova = {
+  slug: 'arbitrumNova',
+  ...config_arbitrumNova,
+  earliestBlock: typedEarliestBlocks['arbitrumNova'],
+}
+
+export const base = {
+  slug: 'base',
+  ...config_base,
+  earliestBlock: typedEarliestBlocks['base'],
+}
+
+export const baseSepolia = {
+  slug: 'baseSepolia',
+  ...config_baseSepolia,
+  earliestBlock: typedEarliestBlocks['baseSepolia'],
+}
+
+export const bob = {
+  slug: 'bob',
+  ...config_bob,
+  earliestBlock: typedEarliestBlocks['bob'],
+}
+
+export const cyber = {
+  slug: 'cyber',
+  ...config_cyber,
+  earliestBlock: typedEarliestBlocks['cyber'],
+}
+
+export const ethereum = {
+  slug: 'ethereum',
+  ...config_ethereum,
+  earliestBlock: typedEarliestBlocks['ethereum'],
+}
+
+export const hemi = {
+  slug: 'hemi',
+  ...config_hemi,
+  earliestBlock: typedEarliestBlocks['hemi'],
+}
+
+export const lisk = {
+  slug: 'lisk',
+  ...config_lisk,
+  earliestBlock: typedEarliestBlocks['lisk'],
+}
+
+export const mint = {
+  slug: 'mint',
+  ...config_mint,
+  earliestBlock: typedEarliestBlocks['mint'],
+}
+
+export const optimism = {
+  slug: 'optimism',
+  ...config_optimism,
+  earliestBlock: typedEarliestBlocks['optimism'],
+}
+
+export const redstone = {
+  slug: 'redstone',
+  ...config_redstone,
+  earliestBlock: typedEarliestBlocks['redstone'],
+}
+
+export const sepolia = {
+  slug: 'sepolia',
+  ...config_sepolia,
+  earliestBlock: typedEarliestBlocks['sepolia'],
+}
+
+export const soneium = {
+  slug: 'soneium',
+  ...config_soneium,
+  earliestBlock: typedEarliestBlocks['soneium'],
+}
+
+export const swell = {
+  slug: 'swell',
+  ...config_swell,
+  earliestBlock: typedEarliestBlocks['swell'],
+}
+
+export const zero = {
+  slug: 'zero',
+  ...config_zero,
+  earliestBlock: typedEarliestBlocks['zero'],
+}
+
+export const zksync = {
+  slug: 'zksync',
+  ...config_zksync,
+  earliestBlock: typedEarliestBlocks['zksync'],
+}
+
+export const zora = {
+  slug: 'zora',
+  ...config_zora,
+  earliestBlock: typedEarliestBlocks['zora'],
+}

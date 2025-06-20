6c754c914cd07fb13e68dbba32d0108f4586a6ec
Julien Genestoux <17735+julien51@users.noreply.github.com>
2025-06-20 10:52:23 -0400

    Update smart-contracts/tasks/deploy/origin-curator.ts

--- smart-contracts/tasks/deploy/origin-curator.ts
+++ smart-contracts/tasks/deploy/origin-curator.ts
@@ -4,7 +4,7 @@ import { networks } from '@relay-vaults/networks'
 import OriginCuratorModule from '../../ignition/modules/OriginCuratorModule'
 
 task('deploy:origin-curator', 'Deploy the Origin Curator')
-  .addOptionalParam('multisig', 'The multisig address')
+  .addParam('multisig', 'The multisig address')
   .setAction(async ({ multisig }, { ethers, ignition, run }) => {
     // get args value
     const { chainId } = await ethers.provider.getNetwork()

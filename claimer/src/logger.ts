cbe12f680b9d9870e21b3896a252592ed0b72d05
Clément Renaud <clement+git@clementrenaud.com>
2025-06-24 17:04:00 +0200

    log directly with console to stdout

--- claimer/src/logger.ts
+++ claimer/src/logger.ts
@@ -4,20 +4,5 @@ export const logger = createLogger({
   defaultMeta: { service: 'vaults-claimer' },
   format: format.json(),
   level: 'info',
-  transports: [
-    new transports.File({
-      filename: 'vaults-claimer-error.log',
-      level: 'error',
-    }),
-    new transports.File({ filename: 'vaults-claimer-combined.log' }),
-  ],
+  transports: [new transports.Console()],
 })
-
-// also node to console if we are not in prod
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(
-    new transports.Console({
-      format: format.combine(format.colorize(), format.simple()),
-    })
-  )
-}

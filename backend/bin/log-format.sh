827f9d3fa24bbbfb889a1038da330812443dafff
Clément Renaud <clement+git@clementrenaud.com>
2025-06-24 17:32:16 +0200

    backend custom log formatter

---
+++ backend/bin/log-format.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script reads input from pipe and update json "service" field with a prefix
+LOG_PREFIX="vaults-backend-"
+
+while IFS=$'\n' read -r line; do
+    # add prefix to service field
+    service="$LOG_PREFIX$(echo $line | jq -r '.service')"
+    echo $line | jq -r --arg a "$service" '.service = ($a) | tostring' 
+done

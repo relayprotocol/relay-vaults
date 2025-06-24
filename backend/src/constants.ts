bf7dd37ad78fddf2c47d3ef75618960ea32874c7
0xtxbi <techiejossy@gmail.com>
2025-06-24 21:33:08 +0100

    add fractional bps to constants

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -1,1 +1,7 @@
 export const SEVEN_DAYS = 60 * 60 * 24 * 7
+
+/*
+ * FRACTIONAL_BPS_DENOMINATOR from RelayPool.sol
+ * Used for fractional basis points calculations
+ */
+export const FRACTIONAL_BPS_DENOMINATOR = 100_000_000_000n

6ec845def23a609b2d1abd848f643a3d1e900792
Clément Renaud <clement+git@clementrenaud.com>
2025-06-24 16:57:11 +0200

    slightly more explicit

--- smart-contracts/lib/zksync.ts
+++ smart-contracts/lib/zksync.ts
@@ -60,7 +60,7 @@ export const verifyContract = async ({
   while (tries < 5) {
     const fullyQualifiedNames = await hre.artifacts.getAllFullyQualifiedNames()
     const fullyQualifiedName = fullyQualifiedNames.find(
-      (d) => d.split(':')[1] === 'RelayBridgeFactory'
+      (q) => q.split(':')[1] === 'RelayBridgeFactory'
     )
     try {
       await run('verify:verify', {

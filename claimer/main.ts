4442d2318e75241576d68fac53582e41a9d76ca8
Julien Genestoux <17735+julien51@users.noreply.github.com>
2025-06-30 14:59:40 -0400

    adding a check on the outstanding debt

--- claimer/main.ts
+++ claimer/main.ts
@@ -3,10 +3,12 @@ import { proveTransactions } from './src/prove-transactions'
 import { claimTransactions } from './src/claim-withdrawals'
 import { finalizeWithdrawals } from './src/finalize-withdrawals'
 import { checkL2Chains } from './src/check-l2-status'
+import { checkOutstandingDebts } from './src/check-outstading-debts'
 import { logger } from './src/logger'
 
 const run = async () => {
   const { vaultService } = await start()
+  await checkOutstandingDebts({ vaultService })
   await proveTransactions({ vaultService })
   await finalizeWithdrawals({ vaultService })
   await claimTransactions({ vaultService })
---
+++ claimer/src/check-outstading-debts.ts
@@ -0,0 +1,41 @@
+import { gql } from 'graphql-request'
+import { logger } from './logger'
+
+const GET_OUTSTANDING_DEBTS = gql`
+  query {
+    relayPools {
+      items {
+        chainId
+        contractAddress
+        outstandingDebt
+        origins {
+          items {
+            currentOutstandingDebt
+          }
+        }
+      }
+    }
+  }
+`
+
+export const checkOutstandingDebts = async ({ vaultService }) => {
+  const { relayPools } = await vaultService.query(GET_OUTSTANDING_DEBTS)
+  for (let i = 0; i < relayPools.items.length; i++) {
+    const { outstandingDebt, origins, chainId, contractAddress } =
+      relayPools.items[i]
+    // verify that it matches the sum of all origins' currentOutstandingDebt
+    const totalOriginOutstandingDebt = origins.items.reduce(
+      (acc, origin) => acc + BigInt(origin.currentOutstandingDebt),
+      BigInt(0)
+    )
+    console.log({ totalOriginOutstandingDebt, outstandingDebt })
+    if (totalOriginOutstandingDebt !== BigInt(outstandingDebt)) {
+      logger.error('debt mismatch', {
+        chainId,
+        contractAddress,
+        outstandingDebt,
+        totalOriginOutstandingDebt,
+      })
+    }
+  }
+}

f8e414cba34b57a4309d6bb1abd6b757b456f3cb
Julien Genestoux <17735+julien51@users.noreply.github.com>
2025-06-30 13:50:57 -0400

    handling transfer of shares (#567)
    * handling transfer of assets
    
    * not throwing when not needed
    
    * linting

---
+++ backend/src/handlers/RelayPool/Transfer.ts
@@ -0,0 +1,62 @@
+import { Context, Event } from 'ponder:registry'
+import { userBalance } from 'ponder:schema'
+import { erc4626Abi, zeroAddress } from 'viem'
+
+export default async function ({
+  event,
+  context,
+}: {
+  event: Event<'RelayPool:Transfer'>
+  context: Context<'RelayPool:Transfer'>
+}) {
+  const timestamp = event.block.timestamp
+  const { to, from, amount: shares } = event.args
+  if (from === zeroAddress || to === zeroAddress) {
+    // Mint/burns are handled via deposits/withdrawals
+    return
+  }
+
+  const assets = await context.client.readContract({
+    abi: erc4626Abi,
+    address: event.log.address,
+    args: [shares],
+    functionName: 'convertToAssets',
+  })
+
+  const user = await context.db.find(userBalance, {
+    chainId: context.network.chainId,
+    relayPool: event.log.address,
+    wallet: event.args.from,
+  })
+
+  // Decrease the share balance of the sender
+  await context.db
+    .update(userBalance, {
+      chainId: context.network.chainId,
+      relayPool: event.log.address,
+      wallet: event.args.from,
+    })
+    .set({
+      lastUpdated: timestamp,
+      shareBalance: user.shareBalance - shares,
+      totalWithdrawn: user.totalWithdrawn + assets,
+    })
+
+  // Increase the share balance of the recipient
+  await context.db
+    .insert(userBalance)
+    .values({
+      chainId: context.network.chainId,
+      lastUpdated: timestamp,
+      relayPool: event.log.address,
+      shareBalance: shares,
+      totalDeposited: assets,
+      totalWithdrawn: 0n,
+      wallet: to,
+    })
+    .onConflictDoUpdate((row) => ({
+      lastUpdated: timestamp,
+      shareBalance: row.shareBalance + shares,
+      totalDeposited: row.totalDeposited + assets,
+    }))
+}

df85d989c4c27e8cf090b769e1e9876e0cf30bfb
Clément Renaud <clement+git@clementrenaud.com>
2025-07-09 22:29:07 +0200

    delete test script

--- packages/client/scripts/test.js
+++
@@ -1,8 +0,0 @@
-// This is what would cause the error in a real CommonJS environment
-const { RelayClient } = require('../dist/index.js')
-
-console.log('Successfully required RelayClient:', typeof RelayClient)
-
-// Try to instantiate the client
-const client = new RelayClient('http://localhost:42069/graphql')
-console.log('Successfully created client instance')

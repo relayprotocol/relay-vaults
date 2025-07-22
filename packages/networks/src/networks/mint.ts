dc1e34987375607a383ce50930da06029ebfe26d
Clément Renaud <clement+git@clementrenaud.com>
2025-07-22 15:05:09 +0200

    mint increase blocks

--- packages/networks/src/networks/mint.ts
+++ packages/networks/src/networks/mint.ts
@@ -12,7 +12,7 @@ const config: OriginNetworkConfig = {
         messagePasser: '0x4200000000000000000000000000000000000016',
       },
       parent: {
-        maxBlocksWithoutProof: 3000,
+        maxBlocksWithoutProof: 3500,
         outputOracle: '0xB751A613f2Db932c6cdeF5048E6D2af05F9B98ED',
         portalProxy: '0x59625d1FE0Eeb8114a4d13c863978F39b3471781',
       },

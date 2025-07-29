c0a954c68f23cc0a11039cdbdc1ea294ff6f6fd2
Clément Renaud <clement+git@clementrenaud.com>
2025-07-29 17:00:40 +0200

    contract draft

---
+++ smart-contracts/contracts/BridgeProxy/withdraw/EverclearBridgeProxy.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {BridgeProxy} from "../BridgeProxy.sol";
+import {IFeeAdapter} from "../../interfaces/everclear/IFeeAdapter.sol";
+
+contract EverclearBridgeProxy is BridgeProxy {
+  address public immutable feeAdapter;
+  uint32 public immutable destinationDomainId;
+
+  constructor(
+    uint256 relayPoolChainId,
+    address relayPool,
+    address l1BridgeProxy,
+    address _feeAdapter,
+    uint32 _destinationDomainId
+  ) BridgeProxy(relayPoolChainId, relayPool, l1BridgeProxy) {
+    feeAdapter = _feeAdapter;
+    destinationDomainId = _destinationDomainId;
+  }
+
+    function bridge(
+    address currency,
+    address l1Asset,
+    uint256 amount,
+    bytes calldata data,
+    bytes calldata extraData
+  ) external payable override {
+
+    // decode fees and sig from extraData
+    (uint256 fee, uint256 deadline, bytes memory sig) = abi.decode(extraData, (uint256, uint256, bytes));
+    IFeeAdapter.FeeParams memory feeParams = IFeeAdapter.FeeParams({
+      fee: fee,
+      deadline: deadline,
+      sig: sig 
+    });
+
+    // parse destination domains
+    uint32[] memory destinations = new uint32[](1);
+    destinations[0] = destinationDomainId;
+
+    // create intent
+    IFeeAdapter(feeAdapter).newIntent(
+      destinations, // destinations
+      address(0), // receiver
+      currency, // inputAsset
+      l1Asset, // outputAsset
+      amount, // amount
+      0, // maxFee
+      0, // ttl
+      data, // data
+      feeParams // feeParams
+    );
+  }
+
+  /// @notice Receives native ETH for bridging operations
+  /// @dev Required to receive ETH
+  receive() external payable {}
+}

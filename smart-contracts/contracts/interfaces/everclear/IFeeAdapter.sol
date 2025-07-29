a3f076614806896ef9897042ddf3e22e7c37db4a
Clément Renaud <clement+git@clementrenaud.com>
2025-07-29 17:01:05 +0200

    everclear intrefaces

---
+++ smart-contracts/contracts/interfaces/everclear/IFeeAdapter.sol
@@ -0,0 +1,305 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+/**
+ * @title IEverclear
+ * @notice Common interface for EverclearHub and EverclearSpoke
+ */
+interface IEverclear {
+  /*//////////////////////////////////////////////////////////////
+                                ENUMS
+    //////////////////////////////////////////////////////////////*/
+  /**
+   * @notice Enum representing statuses of an intent
+   */
+  enum IntentStatus {
+    NONE, // 0
+    ADDED, // 1
+    DEPOSIT_PROCESSED, // 2
+    FILLED, // 3
+    ADDED_AND_FILLED, // 4
+    INVOICED, // 5
+    SETTLED, // 6
+    SETTLED_AND_MANUALLY_EXECUTED, // 7
+    UNSUPPORTED, // 8
+    UNSUPPORTED_RETURNED // 9
+
+  }
+
+  /**
+   * @notice Enum representing asset strategies
+   */
+  enum Strategy {
+    DEFAULT,
+    XERC20
+  }
+
+  /*///////////////////////////////////////////////////////////////
+                            STRUCTS
+  //////////////////////////////////////////////////////////////*/
+
+  /**
+   * @notice The structure of an intent
+   * @param initiator The address of the intent initiator
+   * @param receiver The address of the intent receiver
+   * @param inputAsset The address of the intent asset on origin
+   * @param outputAsset The address of the intent asset on destination
+   * @param maxFee The maximum fee that can be taken by solvers
+   * @param origin The origin chain of the intent
+   * @param destinations The possible destination chains of the intent
+   * @param nonce The nonce of the intent
+   * @param timestamp The timestamp of the intent
+   * @param ttl The time to live of the intent
+   * @param amount The amount of the intent asset normalized to 18 decimals
+   * @param data The data of the intent
+   */
+  struct Intent {
+    bytes32 initiator;
+    bytes32 receiver;
+    bytes32 inputAsset;
+    bytes32 outputAsset;
+    uint24 maxFee;
+    uint32 origin;
+    uint64 nonce;
+    uint48 timestamp;
+    uint48 ttl;
+    uint256 amount;
+    uint32[] destinations;
+    bytes data;
+  }
+
+  /**
+   * @notice The structure of a fill message
+   * @param intentId The ID of the intent
+   * @param solver The address of the intent solver in bytes32 format
+   * @param initiator The address of the intent initiator
+   * @param fee The total fee of the expressed in dbps, represents the solver fee plus the sum of protocol fees for the token
+   * @param executionTimestamp The execution timestamp of the intent
+   */
+  struct FillMessage {
+    bytes32 intentId;
+    bytes32 solver;
+    bytes32 initiator;
+    uint24 fee;
+    uint48 executionTimestamp;
+  }
+
+  /**
+   * @notice The structure of a settlement
+   * @param intentId The ID of the intent
+   * @param amount The amount of the asset
+   * @param asset The address of the asset
+   * @param recipient The address of the recipient
+   * @param updateVirtualBalance If set to true, the settlement will not be transferred to the recipient in spoke domain and the virtual balance will be increased
+   */
+  struct Settlement {
+    bytes32 intentId;
+    uint256 amount;
+    bytes32 asset;
+    bytes32 recipient;
+    bool updateVirtualBalance;
+  }
+}
+interface IFeeAdapter {
+  struct OrderParameters {
+    uint32[] destinations;
+    address receiver;
+    address inputAsset;
+    address outputAsset;
+    uint256 amount;
+    uint24 maxFee;
+    uint48 ttl;
+    bytes data;
+  }
+
+  struct FeeParams {
+    uint256 fee;
+    uint256 deadline;
+    bytes sig;
+  }
+
+  /**
+   * @notice Emitted when a new intent is created with fees
+   * @param _intentId The ID of the created intent
+   * @param _initiator The address of the user who initiated the intent
+   * @param _tokenFee The amount of token fees paid
+   * @param _nativeFee The amount of native token fees paid
+   */
+  event IntentWithFeesAdded(
+    bytes32 indexed _intentId, bytes32 indexed _initiator, uint256 _tokenFee, uint256 _nativeFee
+  );
+
+  /**
+   * @notice Emitted when a new order containing multiple intents is created
+   * @param _orderId The unique identifier for the created order
+   * @param _initiator The address of the user who initiated the order
+   * @param _intentIds Array of intent IDs that make up this order
+   * @param _tokenFee The amount of token fees paid for the order
+   * @param _nativeFee The amount of native token fees paid for the order
+   */
+  event OrderCreated(
+    bytes32 indexed _orderId, bytes32 indexed _initiator, bytes32[] _intentIds, uint256 _tokenFee, uint256 _nativeFee
+  );
+
+  /**
+   * @notice Emitted when the fee recipient is updated
+   * @param _updated The new fee recipient address
+   * @param _previous The previous fee recipient address
+   */
+  event FeeRecipientUpdated(address indexed _updated, address indexed _previous);
+
+  /**
+   * @notice Emitted when the fee signer is updated
+   * @param _updated The new fee signer address
+   * @param _previous The previous fee signer address
+   */
+  event FeeSignerUpdated(address indexed _updated, address indexed _previous);
+
+  /**
+   * @notice Thrown when there are multiple assets included in a single order request.
+   */
+  error MultipleOrderAssets();
+
+  /**
+   * @notice Thrown when the signature is invalid on fees
+   */
+  error FeeAdapter_InvalidSignature();
+
+  /**
+   * @notice Thrown when the deadline has elapsed
+   */
+  error FeeAdapter_InvalidDeadline();
+
+  /**
+   * @notice Returns the spoke contract address
+   * @return The EverclearSpoke contract interface
+   */
+  function spoke() external view returns (address);
+
+  /**
+   * @notice returns the permit2 contract
+   * @return _permit2 The Permit2 singleton address
+   */
+  function PERMIT2() external view returns (address);
+
+  /**
+   * @notice Returns the current fee recipient address
+   * @return The address that receives fees
+   */
+  function feeRecipient() external view returns (address);
+
+  /**
+   * @notice Returns the current fee signer address
+   * @return The address whos signature is verified
+   */
+  function feeSigner() external view returns (address);
+
+  /**
+   * @notice Creates a new intent with fees
+   * @param _destinations Array of destination domains, preference ordered
+   * @param _receiver Address of the receiver on the destination chain
+   * @param _inputAsset Address of the input asset
+   * @param _outputAsset Address of the output asset
+   * @param _amount Amount of input asset to use for the intent
+   * @param _maxFee Maximum fee percentage allowed for the intent
+   * @param _ttl Time-to-live for the intent in seconds
+   * @param _data Additional data for the intent
+   * @param _feeParams Fee parameters including fee amount, deadline, and signature
+   * @return _intentId The ID of the created intent
+   * @return _intent The created intent object
+   */
+  function newIntent(
+    uint32[] memory _destinations,
+    bytes32 _receiver,
+    address _inputAsset,
+    bytes32 _outputAsset,
+    uint256 _amount,
+    uint24 _maxFee,
+    uint48 _ttl,
+    bytes calldata _data,
+    FeeParams calldata _feeParams
+  ) external payable returns (bytes32, IEverclear.Intent memory);
+
+  /**
+   * @notice Creates a new intent with fees
+   * @param _destinations Array of destination domains, preference ordered
+   * @param _receiver Address of the receiver on the destination chain
+   * @param _inputAsset Address of the input asset
+   * @param _outputAsset Address of the output asset
+   * @param _amount Amount of input asset to use for the intent
+   * @param _maxFee Maximum fee percentage allowed for the intent
+   * @param _ttl Time-to-live for the intent in seconds
+   * @param _data Additional data for the intent
+   * @param _feeParams Fee parameters including fee amount, deadline, and signature
+   * @return _intentId The ID of the created intent
+   * @return _intent The created intent object
+   */
+  function newIntent(
+    uint32[] memory _destinations,
+    address _receiver,
+    address _inputAsset,
+    address _outputAsset,
+    uint256 _amount,
+    uint24 _maxFee,
+    uint48 _ttl,
+    bytes calldata _data,
+    FeeParams calldata _feeParams
+  ) external payable returns (bytes32, IEverclear.Intent memory);
+
+  /**
+   * @notice Creates multiple intents with the same parameters, splitting the amount evenly
+   * @dev Creates _numIntents intents with identical parameters but divides _amount equally among them
+   * @param _numIntents Number of intents to create
+   * @param _fee Token fee amount to be sent to the fee recipient
+   * @param _params Order parameters including destinations, receiver, assets, amount, maxFee, ttl, and data
+   * @return _orderId The ID of the created order (hash of all intent IDs)
+   * @return _intentIds Array of all created intent IDs
+   */
+  function newOrderSplitEvenly(
+    uint32 _numIntents,
+    uint256 _fee,
+    uint256 _deadline,
+    bytes calldata _sig,
+    OrderParameters memory _params
+  ) external payable returns (bytes32, bytes32[] memory);
+
+  /**
+   * @notice Creates multiple intents with the supplied parameters
+   * @param _fee Token fee amount to be sent to the fee recipient
+   * @param _params Order parameters including destinations, receiver, assets, amount, maxFee, ttl, and data
+   * @return _orderId The ID of the created order (hash of all intent IDs)
+   * @return _intentIds Array of all created intent IDs
+   */
+  function newOrder(
+    uint256 _fee,
+    uint256 _deadline,
+    bytes calldata _sig,
+    OrderParameters[] memory _params
+  ) external payable returns (bytes32, bytes32[] memory);
+
+  /**
+   * @notice Updates the fee recipient address
+   * @dev Can only be called by the owner of the contract
+   * @param _feeRecipient The new address that will receive fees
+   */
+  function updateFeeRecipient(
+    address _feeRecipient
+  ) external;
+
+  /**
+   * @notice Updates the fee signer address
+   * @dev Can only be called by the owner of the contract
+   * @param _feeSigner The new address that will sign for fees
+   */
+  function updateFeeSigner(
+    address _feeSigner
+  ) external;
+
+  /**
+   * @notice Send virtual balance to the original recipient
+   * @param _asset Address of the asset to return
+   * @param _amount Amount of the asset to return
+   * @param _recipient Address of the recipient
+   */
+  function returnUnsupportedIntent(address _asset, uint256 _amount, address _recipient) external;
+}

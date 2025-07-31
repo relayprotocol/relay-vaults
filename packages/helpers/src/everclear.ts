167ed545e36cd91b703a19889e826c85255221d0
Clément Renaud <clement+git@clementrenaud.com>
2025-07-31 22:04:25 +0200

    add everclear helpers

---
+++ packages/helpers/src/everclear.ts
@@ -0,0 +1,57 @@
+export interface NewIntentParams {
+  origin: string
+  destinations: string[] // dest chain
+  to: string
+  inputAsset: string
+  amount: string | bigint
+  callData: string
+  maxFee: string | bigint
+  permit2Params?: {
+    deadline: string
+    nonce: string
+    signature: string
+  }
+  order_id?: string
+}
+
+export interface NewIntentResponse {
+  to: string
+  from: string
+  nonce: string
+  gasLimit: string
+  gasPrice: string
+  data: string
+  value: string
+  chainId: number
+  type: number
+  accessList: Array<{
+    address: string
+    storageKeys: string[]
+  }>
+  maxPriorityFeePerGas: string
+  maxFeePerGas: string
+  customData: Record<string, unknown>
+  ccipReadEnabled: boolean
+}
+
+// Using everclear API to generate TransactionRequest for a newIntent
+export async function getNewEverclearIntent(
+  params: NewIntentParams
+): Promise<NewIntentResponse> {
+  const response = await fetch('https://api.testnet.everclear.org/intents', {
+    body: JSON.stringify(params),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+  })
+
+  const json = await response.json()
+  if (response.status >= 400) {
+    throw new Error(
+      `API request failed: ${response.status} ${response.statusText}: ${JSON.stringify(json)}`
+    )
+  }
+
+  return json
+}

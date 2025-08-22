8de673a15a7d710a7060f8f6eceb9eccac3b2a7a
Clément Renaud <clement+git@clementrenaud.com>
2025-08-22 22:18:55 +0200

    fix eth > arb bridge proxy amount (#727)
    * only pass arb fee
    
    * take deposit fee from amount
    
    * add full bridge test case
    
    * typo
    
    * update test
    
    * fix script
    
    * fix test

--- packages/helpers/src/arb.ts
+++ packages/helpers/src/arb.ts
@@ -109,10 +109,10 @@ export const estimateNativeBridgeTicketCost = async ({
   )
 
   // compute deposit (sum of all costs, plus l2CallValue)
-  const deposit = gasLimit * maxFeePerGas! + maxSubmissionCost + amount
+  const depositFee = gasLimit * maxFeePerGas! + maxSubmissionCost
 
   return {
-    deposit,
+    depositFee,
     gasLimit,
     maxFeePerGas,
     maxSubmissionCost,

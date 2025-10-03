fdd8b05aa83bbad3723e19487ca40645b9ecf84f
Filipe Mota <filipe@unevenlabs.com>
2025-10-03 16:28:02 +0200

    feat: add support for vault secrets
    Signed-off-by: Filipe Mota <filipe@unevenlabs.com>

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+set -eu
+set -a
+if [ -d /vault/secrets ]; then
+  for f in /vault/secrets/*;
+    do [ -f "$f" ] && . "$f" || true;
+  done
+fi
+set +a
+
 echo "Running command: yarn workspace @relay-vaults/$@"
 
 yarn workspace @relay-vaults/$@
